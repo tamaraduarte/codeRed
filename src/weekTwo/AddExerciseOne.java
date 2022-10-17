2af2ccfb177ae3cbaa13d79c4153ce2d68d4fd21
Tamara Duarte <taminha.larissa@gmail.com>
2022-10-17 08:12:17 +0200

    week 02 - Additional exercise 1

---
+++ src/weekTwo/AddExerciseOne.java
@@ -0,0 +1,110 @@
+package src.weekTwo;
+
+public class AddExerciseOne {
+    public static void main(String[] args) {
+        String character = "X";
+
+        // XXXX   XXXX    XXXXX     XXXXX
+        System.out.print(" ");
+        System.out.print(character + character + character + character + "   ");
+        System.out.print(character + character + character + character + "    ");
+        System.out.print(character + character + character + character + character + "     ");
+        System.out.println(character + character + character + character + character);
+
+        //X       X   X   X    X    X
+        System.out.print(character);
+        System.out.print("       ");
+        System.out.print(character);
+        System.out.print("   ");
+        System.out.print(character);
+        System.out.print("   ");
+        System.out.print(character);
+        System.out.print("    ");
+        System.out.print(character);
+        System.out.print("    ");
+        System.out.println(character);
+
+        // X       X   X   X     X   XXXXX
+        System.out.print(character);
+        System.out.print("       ");
+        System.out.print(character);
+        System.out.print("   ");
+        System.out.print(character);
+        System.out.print("   ");
+        System.out.print(character);
+        System.out.print("     ");
+        System.out.print(character);
+        System.out.print("   ");
+        System.out.println(character + character + character + character + character);
+
+
+        // X       X   X   X    X    X
+        System.out.print(character);
+        System.out.print("       ");
+        System.out.print(character);
+        System.out.print("   ");
+        System.out.print(character);
+        System.out.print("   ");
+        System.out.print(character);
+        System.out.print("    ");
+        System.out.print(character);
+        System.out.print("    ");
+        System.out.println(character);
+
+        // XXXX   XXXX    XXXXX     XXXXX
+        System.out.print(" ");
+        System.out.print(character + character + character + character + "   ");
+        System.out.print(character + character + character + character + "    ");
+        System.out.print(character + character + character + character + character + "     ");
+        System.out.println(character + character + character + character + character);
+
+        System.out.println("   ");
+
+        // XXXXX   XXXX  XXXXX
+        System.out.print(character + character + character + character + character);
+        System.out.print("   ");
+        System.out.print(character + character + character + character);
+        System.out.print("  ");
+        System.out.println(character + character + character + character + character);
+
+        // X    X  X     X    X
+        System.out.print(character);
+        System.out.print("    ");
+        System.out.print(character);
+        System.out.print("  ");
+        System.out.print(character);
+        System.out.print("     ");
+        System.out.print(character);
+        System.out.print("    ");
+        System.out.println(character);
+
+        // XXXXX   XXXX  X     X
+        System.out.print(character + character + character + character + character);
+        System.out.print("   ");
+        System.out.print(character + character + character + character);
+        System.out.print("  ");
+        System.out.print(character);
+        System.out.print("     ");
+        System.out.println(character);
+
+        // X  X    X     X    X
+        System.out.print(character);
+        System.out.print("  ");
+        System.out.print(character);
+        System.out.print("    ");
+        System.out.print(character);
+        System.out.print("     ");
+        System.out.print(character);
+        System.out.print("    ");
+        System.out.println(character);
+
+        // X   X   XXXX  XXXXX
+        System.out.print(character);
+        System.out.print("   ");
+        System.out.print(character);
+        System.out.print("   ");
+        System.out.print(character + character + character + character);
+        System.out.print("  ");
+        System.out.println(character + character + character + character + character);
+    }
+}
