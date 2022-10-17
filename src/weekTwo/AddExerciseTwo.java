ba96098d94b2d2612649b25008fd70bb3bd470be
Tamara Duarte <taminha.larissa@gmail.com>
2022-10-17 16:37:45 +0200

    week 02 - additional exercise 2 - refactoring only one buffer variable needed

--- src/weekTwo/AddExerciseTwo.java
+++ src/weekTwo/AddExerciseTwo.java
@@ -8,12 +8,20 @@ public class AddExerciseTwo {
         System.out.println("Variable 1: " + variableOne); // Variable 1: 5
         System.out.println("Variable 2: " + variableTwo); // Variable 2: 2
 
-
+        /* OLD Version
         int variableOneBuffer = variableOne;
         int variableTwoBuffer = variableTwo;
 
         variableOne = variableTwoBuffer;
         variableTwo = variableOneBuffer;
+
+         */
+
+        int variableOneBuffer = variableOne;
+
+        variableOne = variableTwo;
+        variableTwo = variableOneBuffer;
+
 
         System.out.println("Variable 1: " + variableOne); // Variable 1: 2
         System.out.println("Variable 2: " + variableTwo); // Variable 2: 5
